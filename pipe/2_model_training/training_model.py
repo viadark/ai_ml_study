fc74f4b13f6bc6131ed4e9e33b194db3276fc9d7
minseok2.kang <minseok2.kang@lge.com>
2021-11-10 00:14:43 +0900

    update files

--- pipe/2_model_training/training_model.py
+++ pipe/2_model_training/training_model.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_squared_error
 from io import StringIO
 
 def load_data(data):
@@ -13,14 +13,25 @@ def load_data(data):
     print(boston.shape)
     return boston
 
+def preprocess_data(data):
+    data['ZN'] = data['ZN'].fillna(0)
+    data['NOX'] = data['NOX'].fillna(data.NOX.mean())
+    data.drop(['RAD'], axis='columns', inplace=True)
+    data.drop(['CHAS'], axis='columns', inplace=True)
+    return data
+
+def reshape_data(data):
+    total_cols = len(data.columns)
+    x = data.iloc[:,:-1].values.reshape(-1,total_cols - 1)
+    y = data.iloc[:,-1]
+    return x, y
 
 def get_train_test_data(boston):
     # preprocessing and several data
-    train, test = train_test_split(boston, test_size=0.3)
-    X_train = train
-    X_test = test
-    y_train = []
-    y_test = []
+    total_cols = len(boston.columns)
+    x = boston.iloc[:,:-1].values.reshape(-1,total_cols - 1)
+    y = boston.iloc[:,-1]
+    X_train, X_test, y_train, y_test  = train_test_split(x, y, test_size=0.3)
     return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
@@ -34,8 +45,16 @@ if __name__ == "__main__":
     args = argument_parser.parse_args()
     boston = args.data
     boston = load_data(boston)
+    boston = preprocess_data(boston)
     X_train, X_test, y_train, y_test = get_train_test_data(boston)
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
     model = LinearRegression()
-    model.fit(X_train, y_train)
-    predict = model.predict(X_test)
-    print("accuracy print")
+    model.fit(X_train_scaled, y_train)
+    predict = model.predict(X_test_scaled)
+    from sklearn.metrics import mean_squared_error
+
+    mse = mean_squared_error(y_test, predict)
+    print(f'\nAccuracy Score on test data : {mse}')
