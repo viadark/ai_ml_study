f92b005d5f55407ed15cf3adc1eb3ed1feafc769
minseok2.kang <minseok2.kang@lge.com>
2021-11-10 12:39:09 +0900

    delete boston_contest csv in model training img
    update model training images to 0.2

--- pipe/pipeline.py
+++ pipe/pipeline.py
@@ -18,20 +18,22 @@ def boston_pipeline():
     )
     ml = dsl.ContainerOp(
         name="training pipeline",
-        image="normalboot/boston-train:0.1",
+        image="normalboot/boston-train:0.2",
         arguments=[
             '--data', add_p.outputs['boston']
         ]
     )
-    test = dsl.ContainerOp(
-        name="test pipeline",
-        image="normalboot/boston-test:0.1",
-        arguments=[
-            '--test_data', './boston_contest_test.csv',
-            '--data', ml.outputs['model']
-        ]
-    )
+    
+    # test = dsl.ContainerOp(
+    #     name="test pipeline",
+    #     image="normalboot/boston-test:0.1",
+    #     arguments=[
+    #         '--test_data', './boston_contest_test.csv',
+    #         '--data', ml.outputs['model']
+    #     ]
+    # )
     ml.after(add_p)
+    # test.after(ml)
 
 if __name__ == "__main__":
     import kfp.compiler as compiler
