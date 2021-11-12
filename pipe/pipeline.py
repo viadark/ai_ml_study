8bdaeae3092ffa2813ea7566188a32cf7ccf8317
minseok2.kang <minseok2.kang@lge.com>
2021-11-12 18:11:52 +0900

    add model_deploy and pipeline.py
    1. model_deploy for deploy models to gcp ai models
    2. update pipeline.py for auto upload to kubeflow pipeline

--- pipe/pipeline.py
+++ pipe/pipeline.py
@@ -42,6 +42,15 @@ def boston_pipeline():
             '--answer_data', ans_set.outputs['answer'],
             '--trained_coef', ml.outputs['trained_coef'],
             '--trained_intercept', ml.outputs['trained_intercept']
+        ],
+        file_outputs={'model': '/model.pkl'}
+    )
+
+    deploy = dsl.ContainerOp(
+        name="deploy pipeline to AI Platform",
+        image="normalboot/model-deploy:0.1",
+        arguments=[
+            '--model', test.outputs['model']
         ]
     )
     ml.after(add_p)
@@ -49,6 +58,22 @@ def boston_pipeline():
     test.after(ml)
 
 if __name__ == "__main__":
+    host = "https://123e4a75d6674638-dot-asia-east1.pipelines.googleusercontent.com"
+    pipeline_name = "boston-contest-pipeline"
+    namespace = "boston"
+    pipeline_package_path = "pipeline.zip"
+    version = "v0.1"
+
+    experiment_name = "for dev"
+    run_name = "for dev run"
+
+    client = kfp.Client(host=host, namespace=namespace)
     import kfp.compiler as compiler
-    compiler.Compiler().compile(boston_pipeline, __file__ + ".tar.gz")
-    
+    compiler.Compiler().compile(boston_pipeline, pipeline_package_path)
+    pipeline_id = client.get_pipeline_id(pipeline_name)
+    if pipeline_id:
+        client.upload_pipeline_version(pipeline_package_path=pipeline_package_path, pipeline_version_name=version, pipeline_name=pipeline_name)
+    else:
+        client.upload_pipeline(pipeline_package_path=pipeline_package_path, pipeline_name=pipeline_name)
+    experiment = client.create_experiment(name=experiment_name, namespace=namespace)
+    run = client.run_pipeline(experiment.id, run_name, pipeline_package_path)
