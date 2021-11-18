951219a0bdd74b9e5832f9b5f8e05701c30606c7
viadark <viadark123@gmail.com>
2021-11-18 18:29:08 +0900

    update predict model code

--- flask/apis/list_models.py
+++ flask/apis/list_models.py
@@ -20,11 +20,18 @@ class Models(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str)
         parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+        parser.add_argument('scaler', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
         target_name = args['name']
         print(args['name'])
         #target_name = "boston_model"
-        base_path = f"./models/{target_name}/"
+        print(__file__)
+        print(os.path.realpath(__file__))
+        print(os.path.abspath(__file__))
+        print(os.path.dirname(__file__))
+        base_path = f"{os.path.dirname(__file__)}/../models/{target_name}/"
+        #realpath = os.path.abspath(__file__).split("/")
+        #base_path = f"./models/{target_name}/"
         if not os.path.exists(os.path.dirname(base_path)):
             try:
                 os.makedirs(os.path.dirname(base_path))
@@ -34,6 +41,12 @@ class Models(Resource):
 
         with open(f'{base_path}/model.pkl', "wb") as f:
             f.write(args['file'].read())
+        
+        if args['scaler'] != None:
+            with open(f'{base_path}/scaler.pkl', "wb") as f:
+                f.write(args['scaler'].read())
+        else:
+            print("scaler is null")
         
         print('model saved')
         return 'model saved'
--- flask/apis/predict_model.py
+++ flask/apis/predict_model.py
@@ -4,20 +4,31 @@ import os
 import pandas as pd
 import importlib
 import pickle
+import json
+from flask import jsonify
 
 class PredictModel(Resource):
     def post(self, model_name):
+        print(f"predict_model at {model_name}")
         parser = reqparse.RequestParser()
         parser.add_argument('data', action='append')
         args = parser.parse_args()
-        data = pd.read_csv(args['data'])
-        total_cols = len(data.columns)
-        x = data.iloc[:,:-1].values.reshape(-1,total_cols - 1)
-        print(f'input data : {data}')
-        
-        base_path = f'./model/{model_name}'
-        
+        print(__file__)
+        print(os.path.realpath(__file__))
+        print(os.path.abspath(__file__))
+        print(f"data : {args['data']}")
+        base_path = f"{os.path.dirname(__file__)}/../models/{model_name}"
+        print(base_path)
+        data = pd.DataFrame(args['data'])
+        x = data.iloc[:,:].values.reshape(-1,13)
+        print(x)
+        # total_cols = len(data)
+        # x = data.iloc[:,:-1].values.reshape(-1,total_cols)
+        # print(f"reshape data : {x}")
+        scaler = pickle.load(open(f'{base_path}/scaler.pkl', 'rb'))
         model = pickle.load(open(f'{base_path}/model.pkl', 'rb'))
+        x = scaler.transform(x)
         ret = model.predict(x)
+        res = { "res" : str(ret)}
         print(ret)
-        return ret
+        return json.dumps(res)
